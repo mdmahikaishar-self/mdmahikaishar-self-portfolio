0fca9548bfd4ec2e948647f7e77bb92c9a2ef52e
ammmdmahikaisharme <ammmdmahikaishar.me@gmail.com>
2023-01-08 00:04:10 -0800

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>AMM Md Mahi Kaishar</title>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="I'm a FullStack Developer. Also a programmer. I have done lots of projects. I have been coding for a wheel."
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
